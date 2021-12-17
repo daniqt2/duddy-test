9ef7d1bde462d362c51afd51b8fc03e59796b06f
Daniela Quesada <dquesada@emailmeter.com>
2021-12-17 21:30:46 +0100

    changed folder

--- prueba-back/index.js
+++ prueba-back/index.js
@@ -4,11 +4,17 @@ const cors = require('cors')
 const app = express();
 app.use(cors());
 
-const clientsEp = require('./api/clients.js')
-const trainersEp = require('./api/trainers')
+const clientsEp = require('./api/clients.js');
+const trainersEp = require('./api/trainers');
 
-app.use('/api/clients', clientsEp)
-app.use('/api/trainers', trainersEp)
+app.use('/api/clients', clientsEp);
+app.use('/api/trainers', trainersEp);
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(__dirname+'/public'));
+
+    app.get(/.*/,(req,res)=> res.sendFile(__dirname+'/public/index.html'));
+}
 
 const PORT = process.env.PORT || 3000;
 
